417c469600d1e25c231c84f599d79d359e1dd04c
HarshVisaria <harshvisaria2598@gmail.com>
2024-08-26 00:00:23 +0530

    Add cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContentSectionComponent } from './components/content-section/content-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContentSectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/content-section/content-section.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Item {
+  pk: number;
+  name: string;
+  points: number;
+  display_img_url: string;
+  quantity: number;
+  valid_until: string;
+  low_quantity: number;
+}
+
+@Component({
+  selector: 'app-content-section',
+  templateUrl: './content-section.component.html',
+  styleUrls: ['./content-section.component.scss'],
+})
+export class ContentSectionComponent implements OnInit {
+  items: Item[] = [];
+  availableCategories = [
+    'e-Voucher',
+    'Products',
+    'Evergreen',
+    'Fashion & Retail',
+  ];
+  selectedCategories: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        pk: 987,
+        name: 'Grab $10',
+        points: 150,
+        display_img_url:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png',
+        quantity: 14,
+        valid_until: '2024-12-31T00:00:00',
+        low_quantity: 10,
+      },
+      {
+        pk: 2,
+        name: 'Decathlon Rs.500',
+        points: 10,
+        display_img_url:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png',
+        quantity: 14,
+        valid_until: '2024-12-31T00:00:00',
+        low_quantity: 4,
+      },
+      {
+        pk: 3,
+        name: 'Go HELLLO 20% Off',
+        points: 0,
+        display_img_url:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png',
+        quantity: 0,
+        valid_until: '2024-12-31T00:00:00',
+        low_quantity: 2,
+      },
+      {
+        pk: 4,
+        name: 'Beard Trim 01',
+        points: 0,
+        display_img_url: '',
+        quantity: 1,
+        valid_until: '2024-12-31T00:00:00',
+        low_quantity: 2,
+      },
+    ];
+  }
+
+  selectCategory(category: string): void {
+    const index = this.selectedCategories.indexOf(category);
+    if (index > -1) {
+      // If already selected, remove it
+      this.selectedCategories = [];
+    } else {
+      // Otherwise, add it to the selected categories
+      this.selectedCategories = [category];
+    }
+  }
+}
